809f408df5e3d85e522083f8257c01271f895411
pilgrim <piligrimcheg@gmail.com>
2013-04-24 23:21:29 +0300

    Finish sign in

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,1 +1,21 @@
 include ApplicationHelper
+
+def valid_signin_authentication(user)
+   fill_in "Email", with: user.email
+   fill_in "Password", with: user.password
+   click_button "Sign in"
+end
+
+RSpec::Matchers.define :have_error_message do |message|
+  match do |page|
+    page.should have_selector('div.alert.alert-error', text: message)
+  end
+
+ def valid_signin_user_pages(user)
+      fill_in "Name", with: "Example User"
+      fill_in "Email", with: "user@example.com"
+      fill_in "Password", with: "Foobar"
+      fill_in "Confirmation", with: "Foobar"
+  end
+
+end
